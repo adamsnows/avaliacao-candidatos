39046ae8573f25a82c992ad249c881568bc5f290
Adam N <adaamangelow@gmail.com>
2024-08-28 17:08:23 -0300

    feat: comigotech vercel app cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://comigotech-test.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
